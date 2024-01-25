8d465576342873f46c4bfa90331d07731489f015
ameliadp2002 <amelia.de.paepe@student.howest.be>
2024-01-26 00:29:29 +0100

    css of second part

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 import './css/reset.css'
 import './css/style.css'
+
+// main.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  function toggleAudio() {
+    var audio = document.querySelector('.audio-player audio');
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+
+  // You can also use event delegation to handle the click event on the play button
+  document.querySelector('.audio-player button.play-button').addEventListener('click', toggleAudio);
+});

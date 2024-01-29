45cafd0b6424dbbae97a9f335063919b50502ca7
ameliadp2002 <amelia.de.paepe@student.howest.be>
2024-01-29 20:22:37 +0100

    new play button

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 import './css/reset.css'
 import './css/style.css'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// main.js
+/*gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".desk__train", {
+  x:50,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".roll",
+    start: "top 70%",
+    end: "bottom 20%",
+    toggleClass: "white",
+    markers: { fontSize: "25px", fontWeight: "bold" },
+    scrub: 0.5, //true
+  },
+});*/
+
 
 document.addEventListener("DOMContentLoaded", function() {
   // Define toggleAudio in the global scope
   window.toggleAudio = function(element) {
-    var audio = element.parentElement.querySelector('audio');
+    let audio = element.parentElement.querySelector('audio');
+    let img = element.querySelector('.play__img');
+    let iframe = element.querySelector('.play__hidden');
+
     if (audio.paused) {
       audio.play();
+      img.style.display = 'none';
+      iframe.style.display = 'block';
     } else {
       audio.pause();
+      img.style.display = 'block';
+      iframe.style.display = 'none';
     }
   };
 
+  // Show the audio element initially
+  document.querySelector('.audio-player audio').style.display = 'block';
+
   // Use event delegation to handle the click event on any play button
   document.addEventListener('click', function(event) {
-    var target = event.target;
+    let target = event.target;
 
     // Check if the clicked element has the class 'play-button'
     if (target.classList.contains('play-button')) {
@@ -24,6 +50,3 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
-
-
-
